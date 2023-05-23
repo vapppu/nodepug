ed415401d070321e0504f40b68478aed666a75c6
vapppu <veera.hiltunen@gmail.com>
2023-05-23 19:29:43 +0300

    Add helmet

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
 
 
 app.set('view engine', 'pug');
